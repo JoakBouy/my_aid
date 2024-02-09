199b9d3401ee824d2452ef426cf67f72cca8e095
JoakBouy <b.joak@alustudent.com>
2024-02-09 23:29:01 +0300

    Add welcome page UI updates

--- my_aid/lib/screens/welcome_page.dart
+++ my_aid/lib/screens/welcome_page.dart
@@ -5,9 +5,88 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Welcome to My AID App'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // app bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello,',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Buoy Gai',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // TODO: Add profile picture here
+            
+            Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple[100],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Icon(Icons.person),
+            )
+            
+            // TODO: Add card here
+            SizedBox(height: 20),
+            // TODO: Add search bar here
+            /*
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Search',
+                suffixIcon: Icon(Icons.search),
+              ),
+            ),
+            */
+            // TODO: Add horizontal ListView here
+            /*
+            Container(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  // Add your widgets here
+                ],
+              ),
+            ),
+            */
+            // TODO: Add doctor list here
+            /*
+            ListView.builder(
+              itemCount: doctors.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(doctors[index].name),
+                  subtitle: Text(doctors[index].specialty),
+                );
+              },
+            ),
+            */
+          ],
+        ),
       ),
     );
   }
