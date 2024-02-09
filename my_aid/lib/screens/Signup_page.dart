9cdc4353a64b7d973da62e39200f8e8cccc59710
sharangabo <e.sharangab@alustudent.com>
2024-02-09 18:12:52 +0200

    login page

--- my_aid/lib/screens/Signup_page.dart
+++ my_aid/lib/screens/Signup_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatelessWidget {
-  const WelcomePage({Key? key}) : super(key: key);
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
