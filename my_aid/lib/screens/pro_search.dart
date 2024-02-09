8b5ba4658a93122e1e7b120beb83a54835c8200d
sharangabo <e.sharangab@alustudent.com>
2024-02-09 19:32:15 +0200

    providers search

---
+++ my_aid/lib/screens/pro_search.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProviderSearch extends StatelessWidget {
+  const ProviderSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('it is a search page'),
+      ),
+    );
+  }
+}
