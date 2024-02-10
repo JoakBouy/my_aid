3d177683630fbb67ef717aff3fdc87fcf9d0802f
Oma_goodness <oluomaihejiofor@gmail.com>
2024-02-10 19:47:14 +0300

    confirm booking

---
+++ my_aid/lib/screens/Confirmbooking.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class confirmbooking extends StatelessWidget {
+  const confirmbooking({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Confirmbooking page'),
+      ),
+    );
+  }
+}
