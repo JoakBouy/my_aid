f3a284d0741cbd439c661bd8be7bd1061e97dbb4
Oma_goodness <oluomaihejiofor@gmail.com>
2024-02-10 19:50:15 +0300

    confirm page

---
+++ my_aid/lib/screens/confirm.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class confirmpage extends StatelessWidget {
+  const confirmpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('continue to confirm page'),
+      ),
+    );
+  }
+}
