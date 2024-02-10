4c5037d42de7d439aa2e9478d16511fcdd4c016c
Oma_goodness <oluomaihejiofor@gmail.com>
2024-02-10 19:37:47 +0300

    Forgotpassword

---
+++ my_aid/lib/screens/Forgotpassword.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Forgotpassword extends StatelessWidget {
+  const Forgotpassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('forgotpassword'),
+      ),
+    );
+  }
+}
