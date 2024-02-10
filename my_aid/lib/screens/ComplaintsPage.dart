b4f9c9154f456dde9b2b945d6b7d3a0ac4baf8a4
Oma_goodness <oluomaihejiofor@gmail.com>
2024-02-10 19:52:48 +0300

    complaints page

---
+++ my_aid/lib/screens/ComplaintsPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Complaints extends StatelessWidget {
+  const Complaints({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Complaints Page'),
+      ),
+    );
+  }
+}
